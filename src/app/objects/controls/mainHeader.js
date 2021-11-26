5d328f95c19ea43be1660b3bbe153de53e69700e
Darya Lapitskaya <d.kov0809@gmail.com>
2021-11-26 21:31:17 +0300

    11

---
+++ src/app/objects/controls/mainHeader.js
@@ -0,0 +1,15 @@
+const { element } = require("protractor");
+const BaseObject = require("../baseObject");
+
+class MainHeader extends BaseObject {
+constructor(){
+    super();
+    this.container = element(by.css('.navbar-collapse'));
+
+    //elements
+    this.mainItems = this.container.all(by.css('.navbar-nav>li'));
+
+}
+}
+
+module.exports = MainHeader;
